19858afddbc9e66a69e0c1978f774296adcb8939
powerxpace-cs <66198621+powerxpace-cs@users.noreply.github.com>
2020-07-19 18:41:14 -0400

    Reorganized the code logic

--- the code.py
+++ the code.py
@@ -2,20 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plotgraphs(listxy,
-               listfunc=None,
-               start=-10,
-               stop=10,
-               title="",
-               xlabel="",
-               ylabel=""):
-    x = np.linspace(start, stop)
+def plotlists(listxy):
     for item in listxy:
         plt.plot(item[0], item[1], label=item[2])
+
+
+def plotfunclists(listfunc=None, start=-10, stop=10):
+    x = np.linspace(start, stop)
 
     for item in listfunc:
         plt.plot(item[0](x), item[1](x), label=item[2])
 
+
+def plotgraph(title="Graph", xlabel="X", ylabel="Y"):
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -23,9 +22,10 @@ def plotgraphs(listxy,
     plt.show()
 
 
-plotgraphs([[[1, 2, 3, 4], [8, 7, 5, 4], "line segments"], 
-            [[1, 2, 3, 4, 5], [1, 4, 9, 16, 25], "square"]],
-           [[lambda x: x, lambda x: x**3 - x, "cubic"],
-            [lambda x: x, lambda x: x * x, "computed square"],
-            [lambda x: np.sin(x), lambda x: np.cos(x), "circle"]], -np.pi, np.pi,
-           "graph", "x", "y")
+plotlists([[[1, 2, 3, 4], [8, 7, 5, 4], "line segments"],
+           [[1, 2, 3, 4, 5], [1, 4, 9, 16, 25], "square"]])
+plotfunclists([[lambda x: x, lambda x: x**3 - x, "cubic"],
+               [lambda x: x, lambda x: x * x, "computed square"],
+               [lambda x: np.sin(x), lambda x: np.cos(x), "circle"]], -np.pi,
+              np.pi)
+plotgraph("Demo Figure")
